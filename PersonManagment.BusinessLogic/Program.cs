f049637960eca7474520bec82d13533698fe3b0c
Sharobiddin <sharobiddin@thinkland.uz>
2021-08-19 10:27:00 +0500

    Add project files.

---
+++ PersonManagment.BusinessLogic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonManagment.BusinessLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
